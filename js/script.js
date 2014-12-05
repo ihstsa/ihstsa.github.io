77e01617f1ec3bb3f0ff1abc7a3ddd2a8a0aa54e
Paul F-Y <pbfy00@gmail.com>
2014-12-05 18:32:11 -0500

    nB

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ window.addEventListener('load', function(){
 	var title = document.getElementById('title');
 	window.addEventListener('scroll', function(){
 		if(window.scrollY > title.offsetHeight){
-			title.style.margin-bottom = el.offsetHeight + 'px';
+			title.style.marginBottom = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 		}else{
-			title.style.margin-bottom = 0;
+			title.style.marginBottom = 0;
 			el.style.position = 'initial';
 		}
 	});
