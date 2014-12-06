f1b7ef6ad46153eb789b3420388b22b3930c8e73
Paul F-Y <pbfy00@gmail.com>
2014-12-06 12:34:59 -0500

    IE fix

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function(){
 	var cs = document.defaultView.getComputedStyle(title, '');
 	h = title.offsetHeight + parseFloat(cs.getPropertyValue('margin-top')) + parseFloat(cs.getPropertyValue('margin-bottom'))
 	window.addEventListener('scroll', function(){
-		if(window.scrollY > h){
+		if(document.documentElement.scrollTop > h){
 			header.style.marginBottom = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 			el.style.width = '100%';
