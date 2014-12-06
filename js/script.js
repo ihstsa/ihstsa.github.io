1574a8f3cd9902555e53cbd6b4db24a4ee6f6019
Paul F-Y <pbfy00@gmail.com>
2014-12-06 11:20:28 -0500

    Spacer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 window.addEventListener('load', function(){
 	var el = document.getElementById('links');
 	var title = document.getElementById('title');
+	var spacer = document.getElementById('spacer');
 	var h;
 	var cs = document.defaultView.getComputedStyle(title, '');
 	h = el.offsetHeight + parseFloat(cs.getPropertyValue('margin-top')) + parseFloat(cs.getPropertyValue('margin-bottom'))
 	window.addEventListener('scroll', function(){
 		if(window.scrollY > h){
-			title.style.marginBottom = el.offsetHeight + 'px';
+			spacer.style.height = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 			el.style.width = '100%';
 		}else{
-			title.style.marginBottom = '';
+			spacer.style.height = '';
 			el.style.position = '';
 			el.style.width = '';
 		}
