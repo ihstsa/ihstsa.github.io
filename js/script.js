3129e270c6b6d3b5e0498dbe70b710f454dc64e2
Paul F-Y <pbfy00@gmail.com>
2014-12-06 10:55:37 -0500

    Paragraphs

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ window.addEventListener('load', function(){
 	var el = document.getElementById('links');
 	var title = document.getElementById('title');
 	window.addEventListener('scroll', function(){
-	//return;
 		if(window.scrollY > title.offsetHeight){
 			title.style.marginBottom = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 			el.style.width = '100%';
 		}else{
 			title.style.marginBottom = '';
-			el.style.position = 'initial';
+			el.style.position = '';
 			el.style.width = '';
 		}
 	});
