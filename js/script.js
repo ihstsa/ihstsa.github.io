27ced7654bb61a22d3ca22e47fcec951b56e1ff7
Paul F-Y <pbfy00@gmail.com>
2014-12-06 11:23:10 -0500

    More spacing stuff

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', function(){
 	var el = document.getElementById('links');
 	var title = document.getElementById('title');
-	var spacer = document.getElementById('spacer');
+	var header = document.getElementsByTagName('header')[0];
 	var h;
 	var cs = document.defaultView.getComputedStyle(title, '');
 	h = el.offsetHeight + parseFloat(cs.getPropertyValue('margin-top')) + parseFloat(cs.getPropertyValue('margin-bottom'))
 	window.addEventListener('scroll', function(){
 		if(window.scrollY > h){
-			spacer.style.height = el.offsetHeight + 'px';
+			header.style.marginBottom = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 			el.style.width = '100%';
 		}else{
-			spacer.style.height = '';
+			header.style.marginBottom = '';
 			el.style.position = '';
 			el.style.width = '';
 		}
