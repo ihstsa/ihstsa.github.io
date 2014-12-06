ebd0aac33754d5c78ffa805c56c9ffe4eea72d9d
Paul F-Y <pbfy00@gmail.com>
2014-12-06 11:13:11 -0500

    sdsdf

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 window.addEventListener('load', function(){
 	var el = document.getElementById('links');
 	var title = document.getElementById('title');
+	var h;
+	var cs = document.defaultView.getComputedStyle(title, '');
+	h = el.offsetHeight + cs.getPropertyValue('margin-top') + cs.getPropertyValue('margin-bottom')
 	window.addEventListener('scroll', function(){
-		if(window.scrollY > title.offsetHeight){
+		if(window.scrollY > h){
 			title.style.marginBottom = el.offsetHeight + 'px';
 			el.style.position = 'fixed';
 			el.style.width = '100%';
